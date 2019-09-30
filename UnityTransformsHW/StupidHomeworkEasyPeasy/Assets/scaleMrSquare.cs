ea188784c8b7d4bd4df85565fbcd9f6e4f5f2a8d
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-09-30 11:38:25 -0500

    Added unity project

---
+++ UnityTransformsHW/StupidHomeworkEasyPeasy/Assets/scaleMrSquare.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleMrSquare : MonoBehaviour
+{
+    [SerializeField]
+    private float amount;
+	
+	void Update ()
+    {
+        transform.localScale = new Vector2
+            (transform.localScale.x + amount/2 + Time.deltaTime,
+             transform.localScale.y + amount + Time.deltaTime);
+	}
+}
---
+++ UnityTransformsHW/StupidHomeworkEasyPeasy/Assets/translateMrSquare.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class translateMrSquare : MonoBehaviour
+{
+    [SerializeField]
+    private float bobStrength;
+
+    [SerializeField]
+    private float bobSpeed;
+
+    private float originalPositionX;
+    private float originalPositionY;
+
+    void Start ()
+    {
+        originalPositionX = transform.position.x;
+        originalPositionY = transform.position.y;
+    }
+	
+	void Update ()
+    {
+        transform.position = new Vector2(originalPositionX + ((float)Mathf.Sin(Time.time * bobSpeed) * bobStrength),
+            originalPositionY + ((float)Mathf.Sin(Time.time * bobSpeed) * bobStrength));
+    }
+}
